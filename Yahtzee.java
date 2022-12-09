a082377d33df43d6ee5f4d4f2db07a10f9aacd09
lpeterson52 <111457379+lpeterson52@users.noreply.github.com>
2022-12-09 19:23:54 +0000

    hi

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,13 +1,59 @@
 import java.io.*;
+import java.util.ArrayList;
 public class Yahtzee {
   public int rollsLeft = 3;
   public int lives = 2;
+  private int Dice1 = 0; 
+  private int Dice2 = 0;
+  private int Dice3 = 0; 
+  private int Dice4 = 0;
+  private int Dice5 = 0; 
   private boolean rollDice1 = true;
   private boolean rollDice2 = true;
   private boolean rollDice3 = true;
   private boolean rollDice4 = true;
   private boolean rollDice5 = true;
-  
+
+  ArrayList<Integer> diceRolls = new ArrayList<>();
+    
+//roll method 
+    public void roll(){
+      if(diceRolls.size() == 5){
+        diceRolls.remove(4);
+        diceRolls.remove(3);
+        diceRolls.remove(2);
+        diceRolls.remove(1);
+        diceRolls.remove(0);
+      }
+      if(getrollDice1()){
+        Dice1 = (int)(Math.random()*6) + 1; 
+      }
+      if(getrollDice2()==true){
+        Dice2 = (int)(Math.random()*6) + 1;
+      }
+      if(getrollDice3()==true){
+        Dice3 = (int)(Math.random()*6)+1;
+      }
+      if(rollDice4){
+        Dice4 = (int)(Math.random()*6)+1;
+      }
+      if(rollDice5){
+        Dice5 = (int)(Math.random() * 6 )+ 1;
+      }
+        diceRolls.add(Dice1);
+        diceRolls.add(Dice2);
+        diceRolls.add(Dice3);
+        diceRolls.add(Dice4);
+        diceRolls.add(Dice5);
+  }
+
+
+  public void printRolls(){
+    for(int i = 0; i < diceRolls.size(); i++){
+      System.out.println(diceRolls.get(i));
+    }
+  }
+
 
 
   public void runGame(){
@@ -31,14 +77,13 @@ public class Yahtzee {
   }
   public boolean Threeofakind(){
     int x = 0;
-    //  if (diceRolls[0] = diceRolls[1] || diceRolls[0] = diceRolls[2] || diceRolls[0] = diceRolls[3] || diceRolls[0] = diceRolls[4])
+    //  if (diceRolls[0] == diceRolls[1] || diceRolls[0] == diceRolls[2] || diceRolls[0] == diceRolls[3] || diceRolls[0] == diceRolls[4])
         return false;
   } 
   
   public boolean getrollDice1(){
     return rollDice1;
   }
-
   public boolean getrollDice2(){
     return rollDice2;
   }
